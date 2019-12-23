7bef0303293c45e196baf2d92ea7e616984e77d1
Rivers-Shall <riversshall@qq.com>
2019-12-23 10:13:40 +0800

    Initial Commit

---
+++ HRWebTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HRWebTable
+//
+//  Created by 肖江 on 2019/12/23.
+//  Copyright © 2019 rivers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
